cb9aab7b5a6dd727f4962242e26f5e7fd1e68e54
JangHyeok <janghyeok9257@gmail.com>
2023-02-09 23:36:46 +0900

    redux init

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './samples/node-api'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const store = createStore(rootReducer);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
-postMessage({ payload: 'removeLoading' }, '*')
+postMessage({ payload: "removeLoading" }, "*");
